29647874349d08eee599bb2c3a433810fc642e6e
wenxiaojie <56551200+wenxiaojie@users.noreply.github.com>
2019-12-18 20:07:07 +0800

    Add files via upload

---
+++ try.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import random
+import os
+import torch
+import torchvision
+import detectron2
+from detectron2.utils.logger import setup_logger
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer
+from detectron2.data.datasets import register_coco_instances
+setup_logger()
+register_coco_instances("mydataset_train", {}, "pascal_train.json", "train_images")
+cfg = get_cfg()
+cfg.merge_from_file("/home/wenxiaojie/wenxiaojie_python/深度學習/深度學習HW4/"
+                    "detectron2_repo/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+cfg.MODEL.WEIGHTS = ""
+cfg.DATASETS.TRAIN = ("mydataset_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025
+cfg.SOLVER.MAX_ITER = 30000    # 300 iterations seems good enough, but you can certainly train longer
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 20  # 20 classes
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg) 
+trainer.resume_or_load(resume=False)
+trainer.train()
